cdb19ad22d6acee7d1d93ee99174ffd3d42c3bc8
Renata Silveira <renatamessias.silveira@gmail.com>
2020-01-18 18:45:48 -0200

    Aula!

--- Aulas/basic.py
+++ Aulas/basic.py
@@ -1,1 +1,47 @@
-teste
+Tipos de Dados
+ '''
+ doc string
+'''
+
+print('Curso Python Fundamentals')
+print (100)
+print (1.5)
+
+ usuario = input ("Digite Seu Nome:")
+ print(usuario)
+
+ n1 = int(input("Digite o Primeiro Número: "))
+ n2 = int(input("Digite o Segundo Número: "))
+
+ print (n1 + n2)
+
+ print ("Programa de Notas")
+
+ print ("Informe as notas abaixo:" )
+ n1 = float(input ("Nota 1:"))
+ n2 = float(input ("Nota 2:"))
+ n3 = float(input ("Nota 3:"))
+ n4 = float(input ("Nota 4:"))
+
+media = (n1 + n2 + n3 + n4)/4
+
+print("Média:")
+ print (media)
+
+
+
+lista = ['Corinthians', [1, 2, 3, 4, 5], 'Palmeiras', 'São Paulo', [10, 11, 12, 13, 14],'Flamengo', 'Vasco']
+
+print(lista[1][2], lista[4][3], lista[6])
+
+print(lista[1][3], lista[3], lista[4][4])
+
+'''
+print 4, São Paulo, 14
+'''
+
+print(lista[0], lista[1][1], lista[4][0], lista[4][4])
+
+'''
+print Corinthians, 2, 10, 14
+'''
---
+++ Exercicios/ex_basic.py
@@ -0,0 +1,28 @@
+# Faça um programa que receba 4 notas de alunos
+# some e divida por 4
+
+# print ("Programa de Notas")
+
+# print ("Informe as notas abaixo:" )
+# n1 = float(input ("Nota 1:"))
+# n2 = float(input ("Nota 2:"))
+# n3 = float(input ("Nota 3:"))
+# n4 = float(input ("Nota 4:"))
+
+# media = (n1 + n2 + n3 + n4)/4)
+
+
+# print("Média:")
+# print (media)
+
+# palavra = "Nome sobrenome "
+
+# print(palavra.split())
+
+lista = ['Corinthians', [1, 2, 3, 4, 5] ,'Palmeiras', 'São Paulo', [10, 11, 12, 13, 14],'Flamengo', 'Vasco']
+
+# print 3, 13, vasco
+
+#print 4, São Paulo, 14
+
+# print Corinthians, 2, 10, 14
